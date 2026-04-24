1c1707ae17aeef8ab22fa4e27143af4c51a2e367
mellLuna <nastena.nekrasova.06@bk.ru>
2026-04-24 15:48:26 +0700

    Update main.py
    import csv_manager.route

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from core.database import Base, engine
 from api.v1.endpoints import calls, employees, utils, projects, priorities, statuses
+from csv_manager.router import router as csv_router
 
 # Создаём все таблицы
 Base.metadata.create_all(bind=engine)
@@ -22,6 +23,7 @@ app.include_router(utils.router)
 app.include_router(projects.router)
 app.include_router(priorities.router)
 app.include_router(statuses.router)
+app.include_router(csv_router, prefix="/api/v1")
 
 @app.get("/")
 def root():
